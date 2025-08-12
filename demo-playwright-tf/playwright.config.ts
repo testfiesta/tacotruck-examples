f865c0bf7665d31e4ed917a3b923a95b65cb01f2
chetannn <chetankharel7@gmail.com>
2025-08-12 17:02:06 +0545

    feat: add playwright demo

---
+++ demo-playwright-tf/playwright.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  timeout: 5 * 60 * 1000,
+  reporter: [['html'], ['junit', { outputFile: 'test-results.xml' }]],
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+});
