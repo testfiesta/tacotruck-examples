a9fc301e64773052889c6311fad3e12c2e462d8c
Chetan <chetankharel7@gmail.com>
2025-08-19 15:49:57 +0545

    feat: add more ci options for gh (#8)

--- demo_elixir_tf/config/test.exs
+++ demo_elixir_tf/config/test.exs
@@ -1,7 +1,9 @@
 import Config
 
 config :junit_formatter,
-  report_dir: "test-reports",
   report_file: "test-results.xml",
+  report_dir: Path.expand("./test-reports"),
+  print_report_file: true,
+  automatic_create_dir?: true,
   include_timestamp: true,
   include_filename?: true
