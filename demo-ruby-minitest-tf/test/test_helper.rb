ca2daee7e8f81534318ff9354ab58e10eabad9a1
chetannn <chetankharel7@gmail.com>
2025-08-29 18:30:43 +0545

    fix: remove output generation script

--- demo-ruby-minitest-tf/test/test_helper.rb
+++ demo-ruby-minitest-tf/test/test_helper.rb
@@ -7,22 +7,3 @@ require "minitest/autorun"
 require "minitest/reporters"
 
 Minitest::Reporters.use! Minitest::Reporters::JUnitReporter.new("test-reports")
-
-# # Configure reporters based on environment
-# if ENV['JUNIT_OUTPUT']
-#   # Use JUnit reporter from minitest-reporters
-#   # Extract directory and filename from the path
-#   junit_path = ENV['JUNIT_OUTPUT']
-#   junit_dir = File.dirname(junit_path)
-  
-#   # Ensure the directory exists
-#   FileUtils.mkdir_p(junit_dir)
-  
-#   Minitest::Reporters.use! [
-#     Minitest::Reporters::SpecReporter.new,
-#     Minitest::Reporters::JUnitReporter.new(junit_dir, false)
-#   ]
-# else
-#   # Use spec-style reporting for development
-#   Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
-# end
